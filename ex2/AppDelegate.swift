2893abb88ec2a1dda00abd32fbefcaba3d4f40f8
Alberto Caro Navarro <albertcaronava@gmail.com>
2018-07-19 15:59:34 -0500

    Añadido Navigation Controller en el AppDelegate

--- ex2/AppDelegate.swift
+++ ex2/AppDelegate.swift
@@ -19,7 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = UDGViewController()
+        window?.rootViewController = UINavigationController(rootViewController: UDGViewController())
+
         return true
     }
 
