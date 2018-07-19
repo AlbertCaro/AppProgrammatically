5ada2738c1a59cbe04e02aa69c603b5607a022f8
alumno <alumno@MacBook-Pro-de-valles-apple-08.local>
2018-07-19 15:31:37 -0500

    Initial commit

---
+++ ex2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ex2
+//
+//  Created by alumno on 19/07/18.
+//  Copyright © 2018 CUValles. All rights reserved.
+//
+
+import UIKit
+
+
+class UDGViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
